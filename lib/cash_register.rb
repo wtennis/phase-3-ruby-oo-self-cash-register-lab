d9e531fcf28657f06004ea9cf5646b7cbfaad9e6
Whiting <whitinghtennis@gmail.com>
2021-08-12 11:30:46 -0700

    commit tree 1be3254b5145e1cc6a206be5e7bc8b2e3071a525

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+class CashRegister
+attr_accessor :discount, :items, :total
 
+
+
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total_array = []
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total_array << price*quantity
+        @total += price*quantity
+        quantity_count = quantity
+        while quantity_count > 0
+            @items << title
+            quantity_count -= 1
+        end
+        # @items.push(title*quantity)
+    end
+
+    def total_array
+        @total.inject(0, :+)
+    end
+
+    def apply_discount 
+        @discount > 0 ?
+            (@total = @total - @total*@discount/100
+            "After the discount, the total comes to $#{@total}.")
+        :
+        "There is no discount to apply."
+    end
+
+    def void_last_transaction
+        @total = @total - @total_array[-1]
+        @total_array.pop
+    end
+
+end
+
+
+
+
+
+# require './lib/cash_register.rb'
